d0bc173bfd6c002fd8fd1fc97c79dd78697f385f
YLING\ZhangYunLing <1461428394@qq.com>
2025-04-07 04:05:04 +0800

    删除不必要的文件

---
+++ src/components/first/FIrstComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{ count }}
+        <button @click="add">点我增加</button>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+function add() {
+    count.value++;
+}
+</script>
+<style lang="css"></style>
